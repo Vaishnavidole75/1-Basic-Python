721722fc47b5ac09cc457841ca6c97a5cec2ad3b
Vaishnavi <101575941+Vaishnavidole75@users.noreply.github.com>
2024-02-23 15:06:55 +0530

    Type_Casting.py
    Type Casting is used in various programming language to convert the data from one form to another. It is use for various purpose and application such as Data Mining in Machine Learning, Database Interaction ,User input validation etc.

---
+++ Tyep_Casting.py
@@ -0,0 +1,62 @@
+#################24/7/2023###########
+
+############# TypeCasting ############
+'''Type casting in Python refers to the process
+ of converting one data type into another'''
+
+x=input('heyy')
+print(x)
+
+
+########### complex Number #########
+
+c1=1
+c2=2j
+print('c1:',c1,'c2:',c2)
+print(type(c2))
+print(type(c1))
+print(c1.real)
+print(c2.imag)
+#
+
+##############
+########## Boolean values ####
+all_ok=True
+print(all_ok)
+all_ok=False
+print(all_ok)
+type(all_ok)
+###########################
+
+#####string into boolean#########
+status=bool(input('ok its confirmed'))
+print(status)
+print(type(status))
+#########################
+#Using Arithmatic operator Conversion
+home=10
+away=15
+print(home+away)
+#########################
+####Arithmatic operator#####
+home=10
+away=15
+print(home+away)
+print(type(home+away))
+######
+print(10-4)
+print(10*4)
+print(type(10-4))
+print(type(10*4))
+#######division#####
+print(100/20)
+print(type(100/20))
+#####To ignore frational part
+print(100//20)
+print(type(100//20))
+#######Modules########
+print('modules division 100%13 is:',100%13)
+print('modulus division 3%2:',3%2)
+###############################
+
+
